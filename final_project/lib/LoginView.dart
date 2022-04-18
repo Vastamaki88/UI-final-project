df070e018ce56da7f322fe2933c05393f912f456
Vastamaki88 <tuomas.salomaki@student.lut.fi>
2022-04-18 19:34:59 +0300

    New views added

---
+++ final_project/lib/LoginView.dart
@@ -0,0 +1,176 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/material.dart';
+import 'RegisterView.dart';
+
+
+class LoginView extends StatefulWidget {
+  const LoginView({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<LoginView> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginView> {
+  final _formKey = GlobalKey<FormState>();
+  var rememberValue = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Negotiator App',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    maxLines: 1,
+                    decoration: InputDecoration(
+                      hintText: 'Enter your email',
+                      prefixIcon  : const Icon(Icons.email),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      return null;
+                    },
+                    maxLines: 1,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.lock),
+                      hintText: 'Enter your password',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  CheckboxListTile(
+                    title: const Text("Remember me"),
+                    contentPadding: EdgeInsets.zero,
+                    value: rememberValue,
+                    activeColor: Theme.of(context).colorScheme.primary,
+                    onChanged: (newValue) {
+                      setState(() {
+                        rememberValue = newValue!;
+                      });
+                    },
+                    controlAffinity: ListTileControlAffinity.leading,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {}
+                    },
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(300, 40),
+                       textStyle: const TextStyle(fontSize: 20)
+                                        
+                    ),
+                    child: const Text(
+                      'Log in',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  const RegisterView(title: 'Register UI'),
+                            ),
+                          );
+                        },
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(300, 40),
+                      textStyle: const TextStyle(fontSize: 20)
+                      
+                    ),
+                    
+                    child: const Text(
+                      'Register',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {}
+                    },
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(300, 40),
+                       textStyle: const TextStyle(fontSize: 20)
+                    ),
+                    child: const Text(
+                      'Forgot password or username?',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('Not registered yet?'),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  const RegisterView(title: 'Register UI'),
+                            ),
+                          );
+                        },
+                        child: const Text('Create an account'),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
